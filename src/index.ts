253a8df726c65857fdcf28783781f440973f1254
Keiran McDonald <keza@keza.net>
2018-09-18 21:24:33 +1000

    Get templates showing and posting data back

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,59 @@
-import http from 'http';
-import run from './sheets';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { getInviteData, updateInviteData } from './sheets';
 
 const port = 3000;
+const app = express();
 
-const server = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  console.log('running');
-  await run();
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', async (req, res) => {
+  console.log(`requested root showing index.ejs`);
+  res.render('index');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
+app.get('/invite/:inviteId', async (req, res) => {
+  const inviteId = req.params.inviteId;
+  if (inviteId) {
+    console.log(`requested invite with inviteId: ${inviteId}`);
+    const data = await getInviteData(inviteId);
+    const isValidInvite = data && data.length > 0;
+    if (isValidInvite) {
+      console.log(`showing invite.ejs with data`, data);
+      res.render('invite', { title: 'Hey', data: data });
+      return;
+    }
+  }
+  console.log(`showing index.ejs`);
+  res.render('index');
 });
+
+function transformBody(body) {
+  const result = [];
+  Object.keys(body).map((key) => {
+    const valueArray = Array.isArray(body[key]) ? body[key] : [body[key]];
+    valueArray.map((item, index) => {
+      if (key === 'name') {
+        result.push({ name: item });
+      } else {
+        result[index][key] = item;
+      }
+    });
+  });
+  return result;
+}
+
+app.post('/invite/:inviteId', async (req, res) => {
+  const inviteId = req.params.inviteId;
+  console.log(`posted invite with inviteId: ${inviteId}`);
+  const inviteData = transformBody(req.body);
+  const resultStatus = await updateInviteData(inviteId, inviteData);
+  res.render('thanks', { message: resultStatus });
+});
+
+app.listen(port, () =>
+  console.log(`Server running at http://127.0.0.1:${port}/`),
+);
--- src/sheets/index.ts
+++ src/sheets/index.ts
@@ -1,23 +1,23 @@
-import getInviteData from './get-invite';
-import updateInviteData from './update-invite';
+export { default as getInviteData } from './get-invite';
+export { default as updateInviteData } from './update-invite';
 
-export async function run() {
-  //updates last view and gets current data values
-  const dataArray = await getInviteData('test123');
-  console.log(dataArray);
+// export async function run() {
+//   // gets current data values
+//   const dataArray = await getInviteData('test123');
+//   console.log(dataArray);
 
-  // updates fields and last write time, returns error string or ok
-  const resultStatus = await updateInviteData('test123', [
-    {
-      match: { name: 'Test Name' },
-      data: [{ coming: 'Y' }, { mealChoice: 'Beef' }],
-    },
-    {
-      match: { name: 'Test Name2' },
-      data: [{ coming: 'Y' }, { mealChoice: 'Beef' }],
-    },
-  ]);
-  console.log(resultStatus);
-}
+//   // updates fields and last write time, returns error string or ok
+//   const resultStatus = await updateInviteData('test123', [
+//     {
+//       match: { name: 'Test Name' },
+//       data: [{ coming: 'Y' }, { mealChoice: 'Beef' }],
+//     },
+//     {
+//       match: { name: 'Test Name2' },
+//       data: [{ coming: 'Y' }, { mealChoice: 'Beef' }],
+//     },
+//   ]);
+//   console.log(resultStatus);
+// }
 
-export default run;
+// export default run;
